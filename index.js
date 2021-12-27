20dad63bf4e211961b4fd4b20fa69acf8ad921ef
vasqc <90007988+vasqc@users.noreply.github.com>
2021-12-26 19:16:17 -0500

    questions added

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./');
+const generateMarkdown = require('./utils/generateMarkdowns');
 
 // TODO: Create an array of questions for user input
 //const questions = [];
-const questions = () => {
-return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your name!');
-          return false;
-        }
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your name!');
+        return false;
       }
-    },
-
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your email!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?"
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i'
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?'
+  }
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
