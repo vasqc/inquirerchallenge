93090bb281bddc671b858a6b1a6ac2926a31f4d8
vasqc <90007988+vasqc@users.noreply.github.com>
2021-12-26 22:51:43 -0500

    add path and create read me

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdowns');
+const path = require('path');
 
 // TODO: Create an array of questions for user input
 //const questions = [];
@@ -46,7 +47,7 @@ const questions = [
     type: 'list',
     name: 'license',
     message: 'What kind of license should your project have?',
-    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0','None']
   },
   {
     type: 'input',
@@ -72,7 +73,9 @@ const questions = [
   }
 ];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
